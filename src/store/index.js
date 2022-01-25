7c466edf54e775a5e54406e726b3f1d27f3694ed
buddha <buddhatamang1000@gmail.com>
2022-01-25 19:16:12 +0545

    build  header and auth component

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,11 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    authModelShow: false,
   },
   mutations: {
-  },
-  actions: {
-  },
-  modules: {
+    toggleAuthModal: (state) => {
+      state.authModelShow = !state.authModelShow;
+    },
   },
 });
